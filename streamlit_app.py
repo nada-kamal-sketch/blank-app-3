93aba70e8bc3ca49bfb29db2d34ecc947575b486
nada-kamal-sketch <nhega854@gmail.com>
2024-08-23 19:11:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,5 @@ f2=st.number_input('feature_2',min_value=1,max_value=10)
 f3=st.number_input('feature_3',min_value=1,max_value=10)
 with open('model.pkl','rb')as file:
   model=pickle.load(file)
-  res=model.predict([[f1,f2,f3]])
+res=model.predict([[f1,f2,f3]])
   
