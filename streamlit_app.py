96d1e7181a78e9b8d9d6bcf209f7d0deda444788
nada-kamal-sketch <nhega854@gmail.com>
2024-08-23 18:52:40 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pickle
 
 st.title("🎈 car co2 emssion")
-st.number_input('feature_1',min_value=1,max_value=10)
+f1=st.number_input('feature_1',min_value=1,max_value=10)
+f2=st.number_input('feature_2',min_value=1,max_value=10)
+f3=st.number_input('feature_3',min_value=1,max_value=10)
+with open('model.pkl','rb')as file:
+  
