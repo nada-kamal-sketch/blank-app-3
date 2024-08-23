f3429e4924e21b6962bf3d548ea2501d94e690f7
nada-kamal-sketch <nhega854@gmail.com>
2024-08-23 19:02:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title("🎈 car co2 emssion")
 f1=st.number_input('feature_1',min_value=1,max_value=10)
 f2=st.number_input('feature_2',min_value=1,max_value=10)
 f3=st.number_input('feature_3',min_value=1,max_value=10)
-#with open('model.pkl','rb')as file:
+with open('model.pkl','rb')as file:
+  model=pickle.load(file)
+  res=model.predict([[f1,f2,f3]])
   
